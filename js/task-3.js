57f5ae06401521f054f833d19a6cf20b6c5ee596
Hanna Korol <162054571+HannaKorol@users.noreply.github.com>
2024-07-27 13:12:23 +0200

    added task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,11 @@
 function filterArray(numbers, value) {
-
+const result = [];
+for (const number of numbers) {
+    if (number > value) {
+        result.push(number)
+    }
+}
+return result;
 };
 
 
